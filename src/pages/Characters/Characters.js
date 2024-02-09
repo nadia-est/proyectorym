2575d05e08090e18fa247d9b09ac1bc77522c697
nadia-est <nadiaoviedo577gmail.com>
2024-02-08 22:42:23 -0300

    CAMBIOS2

---
+++ src/pages/Characters/Characters.js
@@ -0,0 +1,6 @@
+export default  function Characters(){
+return(
+  <p>Characters</p>
+  )
+
+}
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,22 @@
+
+import"./Home.css";
+export default  function Home(){
+return(
+    <div className="d-flex justify-content-center aling-items-center vh-100">
+            <div className="bg-home ">
+                <h1 className="text-center">Proyect Rick & Morty</h1>
+                <h2 className="text-center">Welcome to Rick & Morty Proyect!</h2>
+                <p className="text-center">This proyect was made for practising React and to made a functional website.</p>
+                <p className="text-center">In this website you can know information of the characters of this animated series.</p>
+                <p className="text-center">Also you can filter for diferent types of properties to find the character that you are looking for or send us a massage for any concern o sugestion.
+                </p>
+                <p className="text-center">Lets go!</p>
+                <button>Characters</button>
+                <button>Contact</button>        
+            </div>
+
+        </div>
+ 
+  )
+
+}
